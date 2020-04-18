5aed38fb195da305f8f0a53a718176948c5b0cb0
Maurício Dias <mau.dias@gmail.com>
2020-04-19 01:32:18 +0200

    Fixes log group reference

--- main.tf
+++ main.tf
@@ -15,13 +15,9 @@ resource "aws_ecs_task_definition" "task_definition" {
   container_definitions = "${var.task_definition}"
 }
 
-resource "aws_cloudwatch_log_group" "log_group" {
-  name = "${data.aws_cloudwatch_log_group.log_group.lg_prod.name}"
-}
-
 resource "aws_cloudwatch_log_stream" "log_stream" {
   name           = "ls_${var.service_name}"
-  log_group_name = "${aws_cloudwatch_log_group.log_group.name}"
+  log_group_name = "${data.aws_cloudwatch_log_group.log_group.name}"
 }
 
 data "aws_ecs_cluster" "cluster" {
