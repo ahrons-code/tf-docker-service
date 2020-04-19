3e62fb9dce20818a49e6edb72c1677d227c91ab5
Maurício Dias <mau.dias@gmail.com>
2020-04-19 19:45:00 +0200

    Syntax cleanup

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_ecs_service" "service" {
-  name            = "${var.service_name}"
-  cluster         = "${data.aws_ecs_cluster.cluster.id}"
-  task_definition = "${aws_ecs_task_definition.task_definition.arn}"
+  name            = var.service_name
+  cluster         = data.aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
   desired_count   = 1
 
   placement_constraints {
@@ -15,23 +15,23 @@ resource "aws_ecs_service" "service" {
 }
 
 resource "aws_ecs_task_definition" "task_definition" {
-  family = "${var.service_name}"
-  container_definitions = "${data.template_file.init.rendered}"
+  family = var.service_name
+  container_definitions = data.template_file.init.rendered
 }
 
 data "template_file" "init" {
-  template = "${file("${path.module}/service.json")}"
+  template = file("${path.module}/service.json")
   vars = {
-    internal = "${var.internal_port}"
-    external = "${var.external_port}"
-    protocol = "${var.port_protocol}"
+    internal = var.internal_port
+    external = var.external_port
+    protocol = var.port_protocol
     log_stream = "ls_${var.service_name}"
   }
 }
 
 resource "aws_cloudwatch_log_stream" "log_stream" {
   name           = "ls_${var.service_name}"
-  log_group_name = "${data.aws_cloudwatch_log_group.log_group.name}"
+  log_group_name = data.aws_cloudwatch_log_group.log_group.name
 }
 
 data "aws_ecs_cluster" "cluster" {
